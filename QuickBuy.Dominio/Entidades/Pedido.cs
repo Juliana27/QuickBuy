b4c3ae5e3863332914db23bc9a0d527002d219a1
Juliana Oliveira <jujuba_oliveira2@hotmail.com>
2020-05-15 16:53:05 -0300

    Atualização das entidades

--- QuickBuy.Dominio/Entidades/Pedido.cs
+++ QuickBuy.Dominio/Entidades/Pedido.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using QuickBuy.Dominio.ObjetoDeValor;
+using System;
 using System.Collections.Generic;
+using System.Reflection;
 
 namespace QuickBuy.Dominio.Entidades
 {
@@ -8,8 +10,17 @@ namespace QuickBuy.Dominio.Entidades
         public int Id { get; set; }
         public DateTime DataPedido { get; set; }
         public int UsuarioId { get; set; }
-        
-        //Pedido deve ter pelo menos um pedido ou muitos pedidos
+        public DateTime DataPrevisaoEntrega { get; set; }
+        public string CEP { get; set; }
+        public string Estado { get; set; }
+        public string Cidade { get; set; }
+        public string EnderecoCompleto { get; set; }
+        public int NumeroEndereco { get; set; }
+
+        public int FormaPagamentoId { get; set; }
+        public FormaPagamento FormaPagamento { get; set; }
+
+        //Pedido deve ter pelo menos um item de pedido ou muitos itens de pedidos
         public ICollection<ItemPedido> ItensPedidos{ get; set; }
     }
 }
